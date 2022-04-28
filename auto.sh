4764430e946a31eeecd9dca0d4956a4a1dcc68d7
secfan@cpit.me <cpit.me>
2022-04-28 15:42:58 +0800

    *)

---
+++ auto.sh
@@ -0,0 +1,5 @@
+touch 1.md
+date
+echo "Hello World!"
+cd /root/secfan.wiki/
+git pull
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    repo: 'secfan/wiki',
+    repoLabel: '源代码',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: '众人拾柴！'
+  }
+}
