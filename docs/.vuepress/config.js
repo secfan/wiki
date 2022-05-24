38fe3380705b28318534a4b8feaaff8a768b5dfd
secfan@cpit.me <cpit.me>
2022-05-24 22:55:54 +0800

    *)theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,15 @@
 module.exports = {
   themeConfig: {
+    title: 'CPXX-Security-Team',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '安全研究', link: '/study/' }
+    ],
+    sidebar: [
+      '/',
+      '/page-a',
+      ['/page-b', 'Explicit link text']
+    ],
     lastUpdated: 'Last Updated',
     logo: '/assets/img/logo.png',
     repo: 'secfan/wiki',
